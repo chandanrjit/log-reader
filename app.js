0ac759c994d1a5a5a3641941286bfe09e4cbacf2
Chandan Kumar <chandan.kumar@dnata.com>
2018-03-13 21:12:04 +0400

    reinitialized files

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var serveIndex = require('serve-index')
+var index = require('./routes/index')
+var users = require('./routes/users')
+
+var app = express()
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+// Serve URLs like /ftp/thing as public/ftp/thing
+// The express.static serves the file contents
+// The serveIndex is this module serving the directory
+app.use(
+  '/Logger-server-1',
+  express.static('public/ftp'),
+  serveIndex('public/ftp', {
+    icons: true
+  })
+)
+app.use(
+  '/Logger-server-2',
+  express.static('public/ftp2'),
+  serveIndex('public/ftp2', { icons: true })
+)
+app.use('/', index)
+app.use('/users', users)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
